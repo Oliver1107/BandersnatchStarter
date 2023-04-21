4483ab46956dc37ef57addfcdf4e16b44ba9cf9d
Oliver Castellanos-Padilla <castellanosoliver67@gmail.com>
2023-04-21 15:22:18 -0700

    Graph function

--- app/graph.py
+++ app/graph.py
@@ -10,11 +10,7 @@ def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
 
     graph = Chart(
         df[['Level', 'Rarity', 'Health', 'Energy', 'Sanity']],
-        title=f"{y} by {x} for {target}",
-        width=600,
-        height=600,
-        background='#50503c',
-        padding=30
+        title=f"{y} by {x} for {target}"
     ).mark_circle(size=100).encode(
         x=x,
         y=y,
