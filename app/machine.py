e12fc0dc76abd085c52f8e0eedcab48cfb422c02
Oliver Castellanos-Padilla <castellanosoliver67@gmail.com>
2023-05-05 18:18:32 -0700

    machine learning interface

--- app/machine.py
+++ app/machine.py
@@ -1,17 +1,30 @@
+from pandas import DataFrame
+from sklearn.ensemble import RandomForestClassifier
+from datetime import datetime
+import joblib
+
+
 class Machine:
 
-    def __init__(self, df):
-        pass
+    def __init__(self, df: DataFrame):
+        self.name = 'Random Forest Classifier'
+        target = df['Rarity']
+        features = df[['Level', 'Health', 'Energy', 'Sanity']]
+        self.model = RandomForestClassifier()
+        self.model.fit(features, target)
+        self.time = datetime.now()
 
-    def __call__(self, feature_basis):
-        pass
+    def __call__(self, feature_basis: DataFrame):
+        prediction = self.model.predict(feature_basis)[0]
+        confidence = max(self.model.predict_proba(feature_basis)[0])
+        return prediction, confidence
 
-    def save(self, filepath):
-        pass
+    def save(self, filepath: str):
+        joblib.dump(self, filepath)
 
     @staticmethod
-    def open(filepath):
-        pass
+    def open(filepath: str):
+        return joblib.load(filepath)
 
     def info(self):
-        pass
+        return f"Base Model: {self.name}<br>Timestamp: {self.time}"
